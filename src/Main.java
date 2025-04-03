53e403546bc2b3b1696fd10c4c2a135803872a40
Anirudh Jwala <anirudhsai2@gmail.com>
2025-04-04 00:15:02 +0530

    working on main thread methods

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,32 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        // By default, we are given one thread
-        // that is called as DEFAULT thread
-        // that runs the program code
+        /**
+         * By default, we are given one thread
+         * that is called as DEFAULT thread
+         * which runs the program code
+         */
         System.out.println("Application Started...");
 
-        Thread.sleep(5000);
+        // Thread.sleep(5000);
+
+        // Accessing static method of currentThread()
+        Thread thread = Thread.currentThread();
+
+        // Name of DEFAULT thread is "main"
+        System.out.println(thread.getName());
+
+        // Every thread will have a priority ranging from 1-10
+        System.out.println(thread.getPriority());
+
+        Thread.sleep(4000);
+
+        thread.setName("Anirudh");
+        thread.setPriority(4);
+
+        System.out.println(thread.getName());
+        System.out.println(thread.getPriority());
+
+        // Default and user defined threads in java are managed by Thread class
 
         System.out.println("Application Terminated...");
     }
