5310c56d613ea9f883c6df21de6c0b63502b8308
Anirudh Jwala <anirudhsai2@gmail.com>
2025-04-04 00:10:12 +0530

    default thread with sleep

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws InterruptedException {
+        // By default, we are given one thread
+        // that is called as DEFAULT thread
+        // that runs the program code
+        System.out.println("Application Started...");
+
+        Thread.sleep(5000);
+
+        System.out.println("Application Terminated...");
     }
 }
