c65e2089ba2a75b72d8809496f7b749c6edfe3b8
Anirudh Jwala <anirudhsai2@gmail.com>
2025-04-04 23:41:23 +0530

    wrong usage of synchronization leading to deadlock situation

---
+++ src/LaunchThread10.java
@@ -0,0 +1,78 @@
+class Library2 implements Runnable {
+    // Resources (limited)
+    String java = new String("Java");
+    String devOps = new String("DevOps");
+    String aws = new String("Cloud AWS");
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+
+        if (name.equals("Rohan")) {
+            try {
+                System.out.println("Rohan got into Library");
+                Thread.sleep(4000);
+
+                synchronized (aws) {
+                    System.out.println("Rohan has acquired AWS Book");
+                    Thread.sleep(4000);
+
+                    synchronized (devOps) {
+                        System.out.println("Rohan has acquired DevOps Book");
+                        Thread.sleep(4000);
+
+                        synchronized (java) {
+                            System.out.println("Rohan has acquired Java Book");
+                            Thread.sleep(4000);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println("ROHAN Error: " + e.getMessage());
+            }
+        } else {
+            try {
+                System.out.println("Bishnu got into Library");
+                Thread.sleep(4000);
+
+                synchronized (java) {
+                    System.out.println("Bishnu has acquired Java Book");
+                    Thread.sleep(4000);
+
+                    synchronized (devOps) {
+                        System.out.println("Bishnu has acquired DevOps Book");
+                        Thread.sleep(4000);
+
+                        synchronized (aws) {
+                            System.out.println("Bishnu has acquired AWS Book");
+                            Thread.sleep(4000);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println("BISHNU Error: " + e.getMessage());
+            }
+        }
+
+    }
+}
+
+/**
+ * Mutual dependency of resources that will lead
+ * to deadlock situation. When proper usage of synchronization
+ * is not present then we will lead to this.
+ */
+public class LaunchThread10 {
+    public static void main(String[] args) {
+        Library2 lib = new Library2();
+
+        Thread t1 = new Thread(lib);
+        Thread t2 = new Thread(lib);
+
+        t1.setName("Rohan");
+        t2.setName("Bishnu");
+
+        t1.start();
+        t2.start();
+    }
+}
